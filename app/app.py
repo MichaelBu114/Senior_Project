d28069e17d09fc407c810ef16b48308165a4ba4c
MichaelBu114 <56644698+MichaelBu114@users.noreply.github.com>
2020-10-08 18:15:29 -0400

    commit

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 import time
 import redis
 from flask import Flask, render_template
+from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('index.html')
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
