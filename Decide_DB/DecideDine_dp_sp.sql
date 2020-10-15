10ac2bd1020a1f1f5299f5a6fa8b1618f79919a4
Sam Pinizzotto <sampinizzotto@gmail.com>
2020-10-14 20:07:12 -0400

    Update DecideDine_dp_sp.sql
    Added the drop DB and Create (new) Db commands with a USE command.

--- Decide_DB/DecideDine_dp_sp.sql
+++ Decide_DB/DecideDine_dp_sp.sql
@@ -15,6 +15,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP DATABASE IF EXISTS `dp_sp`;
+
+CREATE DATABASE `dp_sp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `dp_sp`;
+
 --
 -- Table structure for table `Categories`
 --
