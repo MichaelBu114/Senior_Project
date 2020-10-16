7300c766b6feb89c88b78dc4d032222acf8300db
MichaelBu114 <56644698+MichaelBu114@users.noreply.github.com>
2020-10-16 14:59:42 -0400

    updated flask with store procedures

--- Webapp/app.py
+++ Webapp/app.py
@@ -19,7 +19,7 @@ def login():
         username = request.form['username'] 
         password = hashlib.md5(request.form['password'])
         cursor = mysql.connect().cursor()
-        cursor.execute('SELECT * FROM Login WHERE username = %s AND password = %s', (username,password,))
+        cursor.callproc('GetLogin', [username,password])
         login = cursor.fetchone()
         if login:
             session['loggedin'] = True
@@ -39,25 +39,28 @@ def logout():
 
 @app.route('/newAccount', methods = ['GET', 'POST'])
 def newAccount():
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'firstName' in request.form and 'lastName' in request.form:
         username = request.form['username'] 
-        password = hashlib.md5(request.form['password']) 
+        password = hashlib.md5(request.form['password'])
+        email = request.form['emai']
+        firstname = request.form['firstName']
+        lastname = request.form['lastName'] 
         cursor = mysql.connect().cursor()
-        cursor.execute('SELECT * FROM login WHERE username = % s', (username, )) 
-        account = cursor.fetchone() 
-        if account: 
-            msg = 'Account already exists !'
+        cursor.callproc('GetUsername', ['username']) 
+        login = cursor.fetchone()
+        if login: 
+            msg = 'Account already exists!'
         elif not re.match(r'[A-Za-z0-9]+', username): 
             msg = 'Username must contain only characters and numbers !'
         elif not username or not password: 
-            msg = 'Please fill out the form !'
+            msg = 'Please fill out the form!'
         else: 
-            cursor.execute('INSERT INTO login VALUES (NULL, % s, % s)', (username, password, )) 
+            cursor.callproc('newUser', [username, password, email, firstname, lastname]) 
             mysql.connection.commit() 
-            msg = 'You have successfully registered !'
+            msg = 'You have successfully registered!'
     elif request.method == 'POST': 
-        msg = 'Please fill out the form !'
+        msg = 'Please fill out the form!'
     return render_template('', msg = msg)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug = True)
