fc8aab398f61d0e629ec29b2eff438f007fe6704
MichaelBu114 <56644698+MichaelBu114@users.noreply.github.com>
2020-10-30 12:31:40 -0400

    fixed small issues

--- Webapp/app.py
+++ Webapp/app.py
@@ -3,19 +3,18 @@ import re
 from flask import Flask, render_template, request, redirect, url_for, session
 from flaskext.mysql import MySQL
 from flask.helpers import flash
-
 import zomato_api
-
 
 app = Flask(__name__)
 mysql = MySQL()
 app.secret_key = 'SeniorProject'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'mysql-development'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'snowflake6365stark'
 app.config['MYSQL_DATABASE_DB'] = 'dp_sp'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql.init_app(app)
+
 
 
 @app.route('/')
@@ -30,13 +29,13 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        cur = mysql.connect().cursor()
-        login = cur.callproc('GetLogin', [username, password])
+        con = mysql.connect()
+        cur = con.cursor()
+        login = cur.callproc('GetLogin', [username,password])
         login = cur.fetchone()
         if login:
             session['logged_in'] = True
             session['username'] = username
-            flash('Login successful')
             return redirect(url_for('home'))
         else:
             error = "Invalid username/password, try again."
@@ -63,13 +62,15 @@ def registration():
         login = cur.fetchone()
         if login: 
             msg = 'Account already exists!'
-        sqlstat = "INSERT INTO Login (username,password,date_changed) VALUES (%s,%s,curdate())"
-        sqlstat2 = "call addUser(%s,%s,%s,LAST_INSERT_ID())"
-        args2 = (firstname,lastname,email)
-        args = (password, email)
-        cur.execute(sqlstat, args)
-        cur.execute(sqlstat2, args2)
-        con.commit()
+        else:
+            sqlstat = "INSERT INTO Login (username,password,date_changed) VALUES (%s,%s,curdate())"
+            sqlstat2 = "call addUser(%s,%s,%s,LAST_INSERT_ID())"
+            args2 = (firstname,lastname,email)
+            args = (email, password)
+            cur.execute(sqlstat, args)
+            cur.execute(sqlstat2, args2)
+            con.commit()
+            return redirect(url_for('search'))
     elif request.method == 'POST': 
         msg = 'Please fill out the form!'
     return render_template('registration.html', msg = msg)
