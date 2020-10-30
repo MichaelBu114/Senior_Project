28305f4c740b78f1f98c720e2b751e7ce8c90e64
MichaelBu114 <56644698+MichaelBu114@users.noreply.github.com>
2020-10-30 10:21:50 -0400

    end-to-end

--- Webapp/app.py
+++ Webapp/app.py
@@ -6,15 +6,14 @@ from flask.helpers import flash
 
 
 app = Flask(__name__)
+mysql = MySQL()
 app.secret_key = 'SeniorProject'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'snowflake6365stark'
 app.config['MYSQL_DATABASE_DB'] = 'dp_sp'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-mysql = MySQL()
 mysql.init_app(app)
-
 
 
 @app.route('/')
@@ -29,10 +28,9 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        db = mysql.connect()
-        cursor = db.cursor()
-        login = cursor.callproc('GetLogin', [username, password])
-        login = cursor.fetchone()
+        cur = mysql.connect().cursor()
+        login = cur.callproc('GetLogin', [username, password])
+        login = cur.fetchone()
         if login:
             session['logged_in'] = True
             session['username'] = username
@@ -42,7 +40,7 @@ def login():
             error = "Invalid username/password, try again."
     return render_template('login.html', error = error)
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
@@ -57,20 +55,22 @@ def registration():
         email = request.form['email']
         firstname = request.form['firstname']
         lastname = request.form['lastname']
-        db = mysql.connect() 
-        cursor = db.cursor()
-        cursor.callproc('GetUsername', [email]) 
-        login = cursor.fetchone()
+        con = mysql.connect()
+        cur = con.cursor()
+        login = cur.callproc('GetUsername', [email]) 
+        login = cur.fetchone()
         if login: 
             msg = 'Account already exists!'
-        elif not email or not password: 
-            msg = 'Please fill out the form!'
-        else: 
-            cursor.callproc('newUser', [password, email, firstname, lastname])
-            return render_template('survey.html')
+        sqlstat = "INSERT INTO Login (username,password,date_changed) VALUES (%s,%s,curdate())"
+        sqlstat2 = "call addUser(%s,%s,%s,LAST_INSERT_ID())"
+        args2 = (firstname,lastname,email)
+        args = (password, email)
+        cur.execute(sqlstat, args)
+        cur.execute(sqlstat2, args2)
+        con.commit()
     elif request.method == 'POST': 
         msg = 'Please fill out the form!'
     return render_template('registration.html', msg = msg)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' ,debug = True)
+    app.run(debug = True)
