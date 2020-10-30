1e3654eed5baa9892fa87d34d4cc0a7bdcc81aeb
John Pansera <panseraj3@students.rowan.edu>
2020-10-30 01:10:53 -0400

    Update zomato_api.py

--- Webapp/zomato_api.py
+++ Webapp/zomato_api.py
@@ -1,19 +1,12 @@
 import sys
 import requests
 
-ZOMATO_API_KEY = "eeb2c8d6b993c20bfd856f1b092ea075 "
-GOOGLE_MAPS_API_KEY = "AIzaSyBTIYFA8avWuLBtGteyCUXhFdDFrqlS648"
-
-ZOMATO_BASE_URL = "https://developers.zomato.com/api/v2.1/search"
-GOOGLE_MAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
-
-# These values will eventually be passed to this script
-ZIP = sys.argv[1] # First argument to script
-RADIUS = 5 # Miles
-SORTING = "real_distance" # Sort by distance
-CUISINES = "" # Filter by one or more cuisines
-
-def test_location_call():
+def search(ZIP, RADIUS, SORTING, CUISINES):
+    ZOMATO_API_KEY = ""
+    GOOGLE_MAPS_API_KEY = ""
+    
+    ZOMATO_BASE_URL = "https://developers.zomato.com/api/v2.1/search"
+    GOOGLE_MAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
     header = {"user-key" : ZOMATO_API_KEY}
     
     # Convert zip code into coordinates
@@ -22,14 +15,14 @@ def test_location_call():
     lon = maps_response["results"][0]["geometry"]["location"]["lng"]
     
     # Call zomato search api
-    meters = RADIUS * 1609
+    meters = int(RADIUS) * 1609
     id = 0
     response_json = {}
     response_json["status"] = "OK"
 
     for start in ["0","21","41","61","81"]:
-        response = requests.get(ZOMATO_BASE_URL+"?lat=%s&lon=%s&radius=%s&sort=%s&cuisines=%s&start=%s&count=20" % (lat, lon, meters, SORTING, CUISINES, start), headers=header).json()
         print ("Calling " + ZOMATO_BASE_URL+"?lat=%s&lon=%s&radius=%s&sort=%s&cuisines=%s&start=%s&count=20" % (lat, lon, meters, SORTING, CUISINES, start))
+        response = requests.get(ZOMATO_BASE_URL+"?lat=%s&lon=%s&radius=%s&sort=%s&cuisines=%s&start=%s&count=20" % (lat, lon, meters, SORTING, CUISINES, start), headers=header).json()
         for i in response["restaurants"]:
             response_json[id] = {} 
             response_json[id]["id"] = i["restaurant"]["id"]
@@ -44,8 +37,6 @@ def test_location_call():
             response_json[id]["menu"] = i["restaurant"]["menu_url"]
             response_json[id]["cuisine"] = i["restaurant"]["cuisines"]
             id += 1
+        response_json['count'] = id
 
-    print ("\n")
-    print (response_json)
-    
-test_location_call()
+    return response_json
