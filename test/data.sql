4fba041abdfeb3cf177b4980c4046e2d77c78c94
jlunsk16 <36424615+jlunsk16@users.noreply.github.com>
2020-11-02 14:22:28 -0500

    committed testing files as well as test users. still work in progress.

---
+++ test/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user(username, password)
+VALUES
+    ('Test1', ) """Not fully finished"""
+    ('testpassword', ) """Not fully finished"""
+
+INSERT INTO post()
+VALUES
+    () """Not fully finished"""
---
+++ test/test.py
@@ -0,0 +1,7 @@
+import unittest
+import flask_testing
+
+# your test cases
+
+if __name__ == '__main__':
+    unittest.main()
